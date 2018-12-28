99f54c842210d99b96d94c76d31a0f2f8cbfde67
yuqiyuqitan <tanyuqi527@gmail.com>
2018-12-28 10:30:36 -0500

    sc_violinClass exportable

--- R/plots.R
+++ R/plots.R
@@ -2088,4 +2088,25 @@ plot.title = element_text(size = base_size, color = "white"),
 ) 
 }
 
+###########theme_dviz_hgrid from https://rdrr.io/github/SimonCoulombe/pkgsimon/src/R/theme_dviz_hgrid.R
+theme_dviz_hgrid <- function(font_size = 14, font_family = "") {
+  color = "grey90"
+  line_size = 0.5
 
+  # Starts with theme_cowplot and then modify some parts
+  theme_cowplot(font_size = font_size, font_family = font_family) %+replace%
+    theme(
+      # make horizontal grid lines
+      panel.grid.major   = element_line(colour = color,
+                                        size = line_size),
+      panel.grid.major.x = element_blank(),
+
+      # adjust axis tickmarks
+      axis.ticks        = element_line(colour = color, size = line_size),
+
+      # adjust x axis
+      axis.line.x       = element_line(colour = color, size = line_size),
+      # no y axis line
+      axis.line.y       = element_blank()
+    )
+}
